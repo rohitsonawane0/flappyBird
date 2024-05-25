9d224d09df31b4bbf53d11ac348ce658b918df8e
rohit sonawane <rohit@icespice.com>
2024-05-25 12:47:37 +0530

    done

--- scenes/PreloadScene.js
+++ scenes/PreloadScene.js
@@ -6,14 +6,14 @@ class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("sky", "assets/sky.png");
-    this.load.spritesheet("bird", "assets/birdSprite.png", {
+    this.load.image("sky", "./assets/sky.png");
+    this.load.spritesheet("bird", "./assets/birdSprite.png", {
       frameWidth: 16,
       frameHeight: 16,
     });
-    this.load.image("pipe", "assets/pipe.png");
-    this.load.image("pause", "assets/pause.png");
-    this.load.image("back", "assets/back.png");
+    this.load.image("pipe", "./assets/pipe.png");
+    this.load.image("pause", "./assets/pause.png");
+    this.load.image("back", "./assets/back.png");
   }
 
   create() {
